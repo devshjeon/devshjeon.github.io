bc0b3e373ca538bc9b94879f71b6929c4fddf544
kane <devshjeon@gmail.com>
2023-06-27 16:33:57 +0900

    feat: base path 설정

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -15,7 +15,7 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 
 (async () => {
   // ensure directory exists
-  const root = "docs"
+  const root = `${process.env.BASE_PATH}/docs`
 
   const databaseId = process.env.DATABASE_ID
   // TODO has_more
