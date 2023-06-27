4fe46f297412ae1989891b66f902f2bdc3cdfa8f
kane <devshjeon@gmail.com>
2023-06-27 17:09:24 +0900

    faet: 롤백

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -15,7 +15,7 @@ const n2m = new NotionToMarkdown({ notionClient: notion });
 
 (async () => {
   // ensure directory exists
-  const root = `${process.env.BASE_PATH}/docs`
+  const root = `_site/docs`
 
   const databaseId = process.env.DATABASE_ID
   // TODO has_more
