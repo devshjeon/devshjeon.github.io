fbb5787e18950756f48e2ea7d9830dc44babd925
kane <devshjeon@gmail.com>
2023-07-03 17:20:27 +0900

    feat: notion 이미지를 aws s3에 업로드하는 방식으로 변경

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -3,6 +3,8 @@ const { NotionToMarkdown } = require("notion-to-md")
 const moment = require("moment")
 const path = require("path")
 const fs = require("fs")
+const https = require("https")
+const AWS = require("aws-sdk")
 // or
 // import {NotionToMarkdown} from "notion-to-md";
 
@@ -10,12 +12,92 @@ const notion = new Client({
   auth: process.env.NOTION_TOKEN,
 })
 
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: "ap-northeast-2",
+})
+
+const s3 = new AWS.S3()
+
 // passing notion client to the option
-const n2m = new NotionToMarkdown({ notionClient: notion });
+const n2m = new NotionToMarkdown({ notionClient: notion })
+const regexPattern = "https:\/\/s3.us-west-2.amazonaws.com.+x-id=GetObject"
+
+function findImageUrl(str) {
+  const regex = new RegExp(regexPattern, "g")
+  const matches = str.match(regex)
+  return matches || []
+}
+
+async function deleteAllFiles(folderPath) {
+  const files = await fs.promises.readdir(folderPath)
+
+  for (let i = 0; i < files.length; i++) {
+    const filePath = path.join(folderPath, files[i])
+    try {
+      await fs.promises.unlink(filePath)
+    } catch (err) {
+      console.error("Error deleting file:", filePath, err)
+    }
+  }
+
+  await fs.promises.rm("_images", { recursive: true })
+}
+
+function downloadImage(url, fileName) {
+  return new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(fileName)
+    https.get(url, (response) => {
+      response.pipe(file)
+      file.on("finish", () => {
+        file.close(resolve)
+      })
+    }).on("error", (err) => {
+      fs.unlink(fileName, () => {
+        reject(err)
+      })
+    })
+  })
+}
+
+async function downloadImages(path, imageUrls) {
+  let number = 1
+  const s3Urls = []
+  for (let url of imageUrls) {
+    const fileName = `${path}/${number}.png`
+    await downloadImage(url, fileName)
+
+    const fileContent = await fs.promises.readFile(fileName)
+    const params = {
+      Bucket: "devshjeon-blog-images",
+      Key: fileName,
+      Body: fileContent,
+    }
+
+    const uploadResult = await s3.upload(params).promise()
+    s3Urls.push(uploadResult.Location)
+    number++
+  }
+
+  await deleteAllFiles(path)
+
+  return s3Urls
+}
+
+function replaceUrl(body, imageUrls, s3Urls) {
+  if (s3Urls.length === imageUrls.length) {
+    for (let i = 0; i < s3Urls.length; i++) {
+      body = body.replace(imageUrls[i], s3Urls[i])
+    }
+  }
+  return body
+}
 
 (async () => {
   // ensure directory exists
   const root = `docs`
+  const imageRoot = "_images"
 
   const databaseId = process.env.DATABASE_ID
   const response = await notion.databases.query({
@@ -102,10 +184,20 @@ parent: ${upFolder}`
 ---`
 
     const folderPath = upFolder ? `${root}/${upUpFolder}/${upFolder}` : `${root}/${upUpFolder}`
+    const imagePath = upFolder ? `${imageRoot}/${upUpFolder}/${upFolder}` : `${imageRoot}/${upUpFolder}`
     fs.mkdirSync(folderPath, { recursive: true })
 
     const mdBlocks = await n2m.pageToMarkdown(id)
-    const body = n2m.toMarkdownString(mdBlocks)["parent"]
+    let body = n2m.toMarkdownString(mdBlocks)["parent"]
+
+    // download image
+    const imageUrls = findImageUrl(body)
+    let s3Urls = []
+    if (imageUrls.length > 0) {
+      fs.mkdirSync(imagePath, { recursive: true })
+      s3Urls = await downloadImages(imagePath, imageUrls)
+      body = replaceUrl(body, imageUrls, s3Urls)
+    }
 
     //writing to file
     const fTitle = navOrder ? `${navOrder}.${title}.md` : `${title}.md`
