8d99b05f7a8c914234b623eed3a81182c789d2d2
kane <devshjeon@gmail.com>
2023-10-29 02:22:09 +0900

    feat: 다크모드 data-theme 적용

--- assets/js/just-the-docs.js
+++ assets/js/just-the-docs.js
@@ -510,43 +510,48 @@ function activateNav() {
   }
 }
 
+function trans() {
+  document.documentElement.classList.add('transition');
+  window.setTimeout(() => {
+    document.documentElement.classList.remove('transition');
+  }, 1000)
+}
+
 function darkMode() {
-  if (localStorage.getItem("theme") === "dark") document.documentElement.classList.add("dark-mode")
   const toggleDarkMode = document.getElementById("theme-toggle")
+  function dark() {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'dark')
+    toggleDarkMode.innerHTML = `<svg width="18px" height="18px"><use href="#svg-moon"></use></svg>`
+    localStorage.setItem("theme", 'dark')
+  }
+
+  function light() {
+    trans()
+    document.documentElement.setAttribute('data-theme', 'light')
+    toggleDarkMode.innerHTML = `<svg width="18px" height="18px"><use href="#svg-sun"></use></svg>`
+    localStorage.setItem("theme", 'light')
+  }
+
+  function getTheme() {
+    return document.documentElement.getAttribute("data-theme") === "dark" ? "dark" : "light"
+  }
 
   if (localStorage.getItem("theme") === "dark") {
-    setTheme("dark")
+    dark()
   } else {
-    setTheme("light")
+    light()
   }
 
   jtd.addEvent(toggleDarkMode, "click", function() {
     const currentTheme = getTheme()
     const newTheme = currentTheme === "dark" ? "light" : "dark"
-
-    localStorage.setItem("theme", newTheme)
-    setTheme(newTheme)
-  })
-
-  function getTheme() {
-    return document.documentElement.classList.contains("dark-mode") ? "dark" : "light"
-  }
-
-  function setTheme(theme = "light") {
-    if (theme === "dark") {
-      toggleDarkMode.innerHTML = `<svg width="18px" height="18px"><use href="#svg-moon"></use></svg>`
-      document.documentElement.classList.add("dark-mode")
-      document.documentElement.classList.remove("light-mode")
+    if (newTheme === 'dark') {
+      dark()
     } else {
-      toggleDarkMode.innerHTML = `<svg width="18px" height="18px"><use href="#svg-sun"></use></svg>`
-      document.documentElement.classList.add("light-mode")
-      document.documentElement.classList.remove("dark-mode")
+      light()
     }
-    const cssFile = document.querySelector("[id=\"main-css\"]")
-    if (!cssFile.href.includes(theme)) {
-      cssFile.setAttribute("href", "/assets/css/just-the-docs-" + theme + ".css")
-    }
-  }
+  })
 }
 
 function tagManager() {
