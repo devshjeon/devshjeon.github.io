0307d5bd3a860c7207216014774d73abfa02b260
devshjeon <devshjeon@gmail.com>
2023-10-31 17:36:08 +0900

    feat: sitemap 자동 생성 로직 추가

--- _scripts/notion-import.js
+++ _scripts/notion-import.js
@@ -7,6 +7,8 @@ const fs = require("fs")
 const https = require("https")
 const AWS = require("aws-sdk")
 const sharp = require("sharp")
+const { SitemapStream, streamToPromise } = require("sitemap")
+const { Readable } = require("stream")
 // or
 // import {NotionToMarkdown} from "notion-to-md";
 
@@ -106,6 +108,17 @@ function replaceUrl(body, imageUrls, s3Urls) {
   return body
 }
 
+function generateSitemap(links) {
+  const stream = new SitemapStream({ hostname: "https://devshjeon.github.io/" })
+  streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
+      fs.writeFile("sitemap.xml", data.toString(), (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    },
+  )
+}
 
 function escapeCodeBlock(body) {
   const regex = /```([\s\S]*?)```/g
@@ -137,103 +150,121 @@ function convertLazyImage(body) {
             equals: true,
           },
         },
-        {
-          property: "배포",
-          checkbox: {
-            equals: true,
-          },
-        },
       ],
     },
   })
+
+  const links = []
+  const priority = 0.8
+  const changeFreq = "daily"
+
   for (const r of response.results) {
     const id = r.id
     let pk = r.properties?.["ID"]?.["unique_id"]?.["number"]
-    // 최상위폴더
-    let upUpFolder = ""
-    let pUpUpFolder = r.properties?.["최상위폴더"]?.["rich_text"]
-    if (pUpUpFolder) {
-      upUpFolder = pUpUpFolder[0]?.["plain_text"]
-    }
 
-    // 상위폴더
-    let upFolder = ""
-    let pUpFolder = r.properties?.["상위폴더"]?.["rich_text"]
-    if (pUpFolder) {
-      upFolder = pUpFolder[0]?.["plain_text"]
-    }
+    // 사이트맵
+    links.push(
+      {
+        url: `/${pk}`,
+        changefreq: changeFreq,
+        priority,
+      },
+    )
 
-    // 순번
-    let navOrder = r.properties?.["순번"]?.["number"] || ""
+    // 배포
+    let isPublished = r.properties?.["배포"]?.["checkbox"] || false
 
-    // 제목
-    let title = id
-    let pTitle = r.properties?.["제목"]?.["title"]
-    if (pTitle?.length > 0) {
-      title = pTitle[0]?.["plain_text"]
-    }
+    // 배포인 경우에만 파일 생성
+    if (isPublished) {
+      // 최상위폴더
+      let upUpFolder = ""
+      let pUpUpFolder = r.properties?.["최상위폴더"]?.["rich_text"]
+      if (pUpUpFolder) {
+        upUpFolder = pUpUpFolder[0]?.["plain_text"]
+      }
 
-    // 메인
-    let hasChild = r.properties?.["메인"]?.["checkbox"] || false
+      // 상위폴더
+      let upFolder = ""
+      let pUpFolder = r.properties?.["상위폴더"]?.["rich_text"]
+      if (pUpFolder) {
+        upFolder = pUpFolder[0]?.["plain_text"]
+      }
 
-    // 작성일
-    let publishedDate = moment(r.created_time).tz("Asia/Seoul").format("YYYY-MM-DD")
-    let modifiedDate = moment(r.last_edited_time).tz("Asia/Seoul").format("YYYY-MM-DD")
-    let header = `---
+      // 순번
+      let navOrder = r.properties?.["순번"]?.["number"] || ""
+
+      // 제목
+      let title = id
+      let pTitle = r.properties?.["제목"]?.["title"]
+      if (pTitle?.length > 0) {
+        title = pTitle[0]?.["plain_text"]
+      }
+
+      // 메인
+      let hasChild = r.properties?.["메인"]?.["checkbox"] || false
+
+      // 작성일
+      let publishedDate = moment(r.created_time).tz("Asia/Seoul").format("YYYY-MM-DD")
+      let modifiedDate = moment(r.last_edited_time).tz("Asia/Seoul").format("YYYY-MM-DD")
+
+      let header = `---
 layout: default
 title: ${title}
 has_children: ${hasChild}
 published_date: ${publishedDate}
 last_modified_date: ${modifiedDate}`
-    if (navOrder) {
-      header += `
+      if (navOrder) {
+        header += `
 nav_order: ${navOrder}`
-    }
+      }
 
-    if (hasChild) {
-      if (upFolder) {
-        header += `
+      if (hasChild) {
+        if (upFolder) {
+          header += `
 parent: ${upUpFolder}`
-      }
-    } else {
-      header += `
-grand_parent: ${upUpFolder}`
-      if (upFolder) {
+        }
+      } else {
         header += `
+grand_parent: ${upUpFolder}`
+        if (upFolder) {
+          header += `
 parent: ${upFolder}`
+        }
       }
-    }
-    header += `
+      header += `
 permalink: '${pk}'`
-    header += `
+      header += `
 ---`
 
-    const folderPath = upFolder ? `${root}/${upUpFolder}/${upFolder}` : `${root}/${upUpFolder}`
-    const imagePath = upFolder ? `${imageRoot}/${upUpFolder}/${upFolder}/${title}` : `${imageRoot}/${upUpFolder}/${title}`
-    fs.mkdirSync(folderPath, { recursive: true })
+      const folderPath = upFolder ? `${root}/${upUpFolder}/${upFolder}` : `${root}/${upUpFolder}`
+      const imagePath = upFolder ? `${imageRoot}/${upUpFolder}/${upFolder}/${title}` : `${imageRoot}/${upUpFolder}/${title}`
+      fs.mkdirSync(folderPath, { recursive: true })
 
-    const mdBlocks = await n2m.pageToMarkdown(id)
-    let body = n2m.toMarkdownString(mdBlocks)["parent"]
+      const mdBlocks = await n2m.pageToMarkdown(id)
+      let body = n2m.toMarkdownString(mdBlocks)["parent"]
 
-    // code block escape
-    body = escapeCodeBlock(body)
+      // code block escape
+      body = escapeCodeBlock(body)
 
-    // download image
-    const imageUrls = findImageUrl(body)
-    if (imageUrls.length > 0) {
-      fs.mkdirSync(imagePath, { recursive: true })
-      const s3Urls = await downloadImages(imagePath, imageUrls)
-      body = replaceUrl(body, imageUrls, s3Urls)
-    }
-
-    body = convertLazyImage(body)
-
-    //writing to file
-    const fTitle = `${pk}.md`
-    fs.writeFile(path.join(folderPath, fTitle), header + body, (err) => {
-      if (err) {
-        console.log(err)
+      // download image
+      const imageUrls = findImageUrl(body)
+      if (imageUrls.length > 0) {
+        fs.mkdirSync(imagePath, { recursive: true })
+        const s3Urls = await downloadImages(imagePath, imageUrls)
+        body = replaceUrl(body, imageUrls, s3Urls)
       }
-    })
+
+      body = convertLazyImage(body)
+
+      //writing to file
+      const fTitle = `${pk}.md`
+      fs.writeFile(path.join(folderPath, fTitle), header + body, (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
   }
+
+  generateSitemap(links)
 })()
